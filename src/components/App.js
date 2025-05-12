e7b17ef5ebf8d1820b2cff79169f2d02918f0f91
Parikshit <Parikshitmali17@gmail.com>
2025-05-12 17:33:17 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [time, setTime]=useState(parseInt(0))
+  console.log(typeof time)
+
+
+ useEffect(()=>{
+ let interval= setInterval(()=>setTime(time+1),1000)
+ return()=>clearInterval(interval)
+ },[time])
+
   return (
     <div>
         {/* Do not remove the main div */}
+
+        <p>You have been on this page for {time} Seconds</p>
     </div>
   )
 }
